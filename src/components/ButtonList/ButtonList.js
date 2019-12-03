fc658836faea2f4f845d55518c89580de73ea1d4
Cameron Newby <cameron@ebi.ai>
2019-12-03 00:03:40 +0000

    Adding tests for stateless/dumb components

--- src/components/ButtonList/ButtonList.js
+++ src/components/ButtonList/ButtonList.js
@@ -4,29 +4,30 @@ import PropTypes from 'prop-types'
 import './ButtonList.scss'
 
 const ButtonList = props => {
+  let buttons
   const [active, setActive] = useState(false)
 
-  return (
-    <div className="buttonList">
-      {props.items.map((button, idx) => (
-        <button
-          className={active === idx ? 'button selected' : 'button'}
-          key={idx}
-          onClick={event => {
-            setActive(idx)
-            props.clicked(event, button)
-          }}
-        >
-          {button}
-        </button>
-      ))}
-    </div>
-  )
+  if (props.items) {
+    buttons = props.items.map((button, idx) => (
+      <button
+        className={active === idx ? 'button selected' : 'button'}
+        key={idx}
+        onClick={event => {
+          setActive(idx)
+          props.clicked(event, button)
+        }}
+      >
+        {button}
+      </button>
+    ))
+  }
+
+  return <div className="buttonList">{buttons}</div>
 }
 
 ButtonList.propTypes = {
-  items: PropTypes.array,
-  clicked: PropTypes.func,
+  items: PropTypes.array.isRequired,
+  clicked: PropTypes.func.isRequired,
 }
 
 export default ButtonList
